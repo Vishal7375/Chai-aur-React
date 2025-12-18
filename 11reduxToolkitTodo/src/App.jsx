35ca20cf696e6cd6e038c33f956ce1bee879761c
Vishal Kubavat <vkubavat77@gmail.com>
2025-12-18 12:20:51 +0530

    redux toolkit project

---
+++ 11reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,15 @@
+import Addtodo from "./components/Addtodo"
+import Todos from "./components/Todos"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-center">Learn about Redux Toolkit</h1>
+      <Addtodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
