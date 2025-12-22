5e000eac905632bd366c5431a70b373f381f7459
Vishal Kubavat <vkubavat77@gmail.com>
2025-12-22 12:21:13 +0530

    store config

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,14 +1,48 @@
+import { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import {login, logout} from './store/authSlice'
+import { Header, Footer } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>A blog app with appwrite</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCuurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}));
+      } else {
+        dispatch(logout()); 
+      }  
+    })
+    .finally(() => setLoading(false));
+  }, []);
+
+  // if (loading) {
+  //   return <div>Loading...</div>;
+  // }
+
+  // return (
+  //   <div className="App">
+  //     <h1>Welcome to 12MegaBlog</h1>
+  //   </div>
+  // )
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+         TODO: <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
